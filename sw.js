5211e8d986ca7ce76c183b9a6ae94c43f9b26260
eugenio3006 <eugenio.liccardi.ansfisa23@gmail.com>
2026-04-16 19:47:36 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE = "rilievo-v1";
-const FILES = ["/", "/index.html", "/manifest.json"];
-
+const FILES = ["/rilievo-difetti/", "/rilievo-difetti/index.html", "/rilievo-difetti/manifest.json"];
+ 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
-
+ 
 self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match("/index.html")))
